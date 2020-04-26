875851a068ffa4196374951b0ad64fa0341c2cfd
Dimytch <dimitri@pekarovsky.name>
2020-04-26 21:02:55 +0300

    Object#try added

--- lib/object.rb
+++ lib/object.rb
@@ -5,4 +5,13 @@ class Object
   def to_a
     [ self ]
   end
+
+  # stolen from activesupport core extensions
+  def try( method_name = nil, *args, &b )
+    if method_name.nil? && block_given?
+      b.arity == 0 ? instance_eval( &b ) : yield( self )
+    elsif respond_to?( method_name )
+      public_send( method_name, *args, &b )
+    end
+  end
 end
